73c9702dc6c5db3c3b4a9ee1b3cba8db51ebcdf4
ikam6 <marko93@bluewin.ch>
2018-10-14 19:43:49 +0200

    Exemple du main.c

--- TP3/main.c
+++ TP3/main.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-	printf("Hello, World!\n");
+// int main()
+// {
+// 	printf("Hello, World!\n");
+// 	return 0;
+// }
+
+extern "C" {
+	#include "module1.h"
+	#include "module2.h"
+}
+
+#define STRING_MAX 30
+
+namespace {
+	char option = "none",
+	char text = "none";
+
+	int calcul = 0;
+}
+int main(int argc, char const *argv[]) {
+	option = recuperation_option();
+	text = recuperation_text();
+
+	calcul = calculhash();
+
 	return 0;
 }
---
+++ TP3/module2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int calculSHA1(/* arguments */) {
+    /* code */
+}
