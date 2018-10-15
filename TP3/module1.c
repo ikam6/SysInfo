17a80001dd3d6409bd319e6f348bca701395be07
alexialid <alexia.lienhard@etu.unige.ch>
2018-10-15 23:08:04 +0200

    j'ai fait qq ajustement visuel, le module1 compile pas tout seul il y a des variables non déclarées genre texte

--- TP3/module1.c
+++ TP3/module1.c
@@ -9,13 +9,17 @@
 
 int recuperation_donnees(int argc, char *argv[])
 {
+  //déclaration des variables
     int option = 0;
     int option_f= 0;
     int option_t= 0;
-
-    char c[]="f:t:";
+    int i;
+    char c[]="ft:";
+    char* Choix;
     int IsOptionHere = 0;
 
+
+//récuperation des options d'entrées
     while ((IsOptionHere = getopt(argc, argv, c))!= -1){
         switch (IsOptionHere) {
             case 'f':
@@ -23,6 +27,7 @@ int recuperation_donnees(int argc, char *argv[])
             break;
             case 't':
             option_t = 1;
+            Choix = optarg;//récupération de l'argument de digest
             break;
             default: /* '?' */
             fprintf(stderr, "Utilisation: %s [-f with file] string [-t with digest]\n",
@@ -30,6 +35,8 @@ int recuperation_donnees(int argc, char *argv[])
             exit(EXIT_FAILURE);
         }
     }
+
+    //message d'erreur s'il n'y a pas d'arguments
     printf("f et t : %i %i\n", option_f, option_t );
     if (argc == 1) {
         fprintf(stderr, "Utilisation: %s [-f with file] string [-t with digest]\n",
@@ -37,30 +44,42 @@ int recuperation_donnees(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    if (option_f == 1 && option_t == 1){
+
+/*si on a les deux options, on doit lire le fichier
+ et prendre l'argument de hash
+  */
+   if (option_f == 1 && option_t == 1){
         option = 22;
+
         //printf("si f et t : %s, %s\n", argv[2], argv[4]);
         //choixHash(option, argv[2], argv[4]);
         FILE* fichier = NULL;
         char text[STRING_MAX] = "";
-        fichier = fopen(argv[2], "r");
-        if (fichier != NULL){
+
+
+          fichier = fopen(argv[2], "r");
+          if (fichier != NULL){
             fgets(text, STRING_MAX, fichier);
             fclose(fichier);
-        }
-        else {
-            printf("Impossible d'ouvrir le fichier %s\n", argv[2]);
-            exit(EXIT_FAILURE);
-        }
-        printf("%s", text);
+          }
+      else
+       {
+        printf("Impossible d'ouvrir le fichier %s\n", argv[2]);
+        exit(EXIT_FAILURE);
+      }
 
+
+        printf("%s", text);
         calculHash(text, argv[4]);
-    } else if (option_f == 1 && option_t == 0){
+
+
+     else if (option_f == 1 && option_t == 0){
         option = 21;
         //printf("si f : %s\n", argv[2]);
         //choixHash(option, argv[2], "sha1");
         FILE* fichier = NULL;
         char text[STRING_MAX] = "";
+
         fichier = fopen(argv[2], "r");
         if (fichier != NULL){
             fgets(text, STRING_MAX, fichier);
@@ -70,10 +89,11 @@ int recuperation_donnees(int argc, char *argv[])
             printf("Impossible d'ouvrir le fichier %s\n", argv[2]);
             exit(EXIT_FAILURE);
         }
-        printf("%s", text);
+        printf("%s\n", text);
 
         calculHash(text, "sha1");
-    } else if (option_f == 0 && option_t == 1){
+      }
+     else if (option_f == 0 && option_t == 1){
         option = 12;
         //choixHash(option, argv[1], argv[3]);
         calculHash(argv[3], argv[2]);
