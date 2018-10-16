176c0bd114c10fc208ce326d5eec264627b12ffb
alexialid <alexia.lienhard@etu.unige.ch>
2018-10-16 10:10:35 +0200

    j'ai mis le module1 en fonction, il y a une erreur qq part mais je sais pas comment la corriger, j'ai pas modifié module1.h

--- TP3/module1.c
+++ TP3/module1.c
@@ -9,17 +9,20 @@
 
 #define STRING_MAX 1000
 
-int recuperation_donnees(int argc, char *argv[])
+char recuperation_options(int argc, char *argv[])
 {
   //déclaration des variables
     int option = 0;
     int option_f= 0;
     int option_t= 0;
-    int i;
-    int *sizeofchain=NULL;
-    char c[]="ft:";
-    char *arr=malloc(1000*sizeof(char));
     char* Choix;
+    int options;
+    char c[]="ft:";
+
+    int *sizeofchain=NULL;
+
+
+
     int IsOptionHere = 0;
     char text[STRING_MAX] = "";
 
@@ -53,20 +56,21 @@ int recuperation_donnees(int argc, char *argv[])
         argv[0]);
         exit(EXIT_FAILURE);
     }
+    options=option_f+option_t;
+     return Choix;
+  }
 
 
 /*si on a les deux options, on doit lire le fichier
  et prendre l'argument de hash
-  */
-   if (option_f + option_t == 4){
-        option = 22;
-
+  */void AllOptions(int argc, char* argv[])
+  {
+    char text[STRING_MAX] = "";
+    int option = 0;
+    option =22;
         //printf("si f et t : %s, %s\n", argv[2], argv[4]);
         //choixHash(option, argv[2], argv[4]);
         FILE* fichier = NULL;
-
-
-
           fichier = fopen(argv[2], "r");
           if (fichier != NULL){
             fgets(text, STRING_MAX, fichier);
@@ -78,15 +82,15 @@ int recuperation_donnees(int argc, char *argv[])
         exit(EXIT_FAILURE);
       }
 
-
         printf("%s", text);
         calculHash(text, argv[4]);
-
 }
 
+void option_f_only(int argc, char* argv[]){
 //si on a que l'option de fichier on doit lire le fichier et faire le hash sha1
-     else if (option_f + option_t == 1){
+        int option = 0;
         option = 21;
+
         //printf("si f : %s\n", argv[2]);
         //choixHash(option, argv[2], "sha1");
         FILE* fichier = NULL;
@@ -104,13 +108,32 @@ int recuperation_donnees(int argc, char *argv[])
         printf("%s\n", text);
 
         calculHash(text, "sha1");
-      }
-     else if (option_f + option_t == 3){
-        option = 12;
-        //choixHash(option, argv[1], argv[3]);
-        calculHash(argv[3], argv[2]);
 
-    } else if (option_f + option_t == 0){
+      }
+
+      void option_t_only(int argc, char* argv[]){
+
+        int i;
+        int Choix= recuperation_options(argc, argv);
+        int option = 0;
+        char *arr=malloc(1000*sizeof(char));
+        option = 12;
+
+
+        for(i=0;i<argc-1;i++){
+            strcat(arr,argv[i+1]);
+            strcat(arr," ");
+}
+        //choixHash(option, argv[1], argv[3]);
+        calculHash(arr, Choix);
+}
+
+
+
+void No_option(int argc, char* argv[]){
+      int option = 0;
+      int i;
+      char *arr=malloc(1000*sizeof(char));
         option = 11;
 
     for(i=0;i<argc-1;i++){
@@ -120,15 +143,7 @@ int recuperation_donnees(int argc, char *argv[])
 
     }
 
-    printf("%s\n",arr);
-
           calculHash(arr, "sha1");
           free(arr);
-        }
 
-     else {
-        option = 0;
-    }
-
-    return option;
 }
