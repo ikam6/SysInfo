8d0dd2bbe028410211bfd241a3164ed0e8029869
alexialid <alexia.lienhard@etu.unige.ch>
2018-10-16 09:15:17 +0200

    j'ai réglé le problème de la chaine de caractère il faut encore faire en sorte que ca marche pour plusieurs fichier mais sinon on est dans le bien :)

--- TP3/module1.c
+++ TP3/module1.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <string.h>
+
 
 #include "module2.h"
 
@@ -14,11 +16,13 @@ int recuperation_donnees(int argc, char *argv[])
     int option_f= 0;
     int option_t= 0;
     int i;
+    int *sizeofchain=NULL;
     char c[]="ft:";
+    char *arr=malloc(1000*sizeof(char));
     char* Choix;
     int IsOptionHere = 0;
     char text[STRING_MAX] = "";
-    
+
 
 //récuperation des options d'entrées
     while ((IsOptionHere = getopt(argc, argv, c))!= -1){
@@ -27,8 +31,13 @@ int recuperation_donnees(int argc, char *argv[])
             option_f = 1;
             break;
             case 't':
-            option_t = 1;
+            option_t = 3;
+
             Choix = optarg;//récupération de l'argument de digest
+            /*if(Choix!="sha1"||"md5"){
+              printf("%s n'est pas un digest accepté\n",Choix );
+              exit(EXIT_FAILURE);*/
+
             break;
             default: /* '?' */
             fprintf(stderr, "Utilisation: %s [-f with file] string [-t with digest]\n",
@@ -49,7 +58,7 @@ int recuperation_donnees(int argc, char *argv[])
 /*si on a les deux options, on doit lire le fichier
  et prendre l'argument de hash
   */
-   if (option_f == 1 && option_t == 1){
+   if (option_f + option_t == 4){
         option = 22;
 
         //printf("si f et t : %s, %s\n", argv[2], argv[4]);
@@ -76,7 +85,7 @@ int recuperation_donnees(int argc, char *argv[])
 }
 
 //si on a que l'option de fichier on doit lire le fichier et faire le hash sha1
-     else if (option_f == 1 && option_t == 0){
+     else if (option_f + option_t == 1){
         option = 21;
         //printf("si f : %s\n", argv[2]);
         //choixHash(option, argv[2], "sha1");
@@ -96,15 +105,28 @@ int recuperation_donnees(int argc, char *argv[])
 
         calculHash(text, "sha1");
       }
-     else if (option_f == 0 && option_t == 1){
+     else if (option_f + option_t == 3){
         option = 12;
         //choixHash(option, argv[1], argv[3]);
         calculHash(argv[3], argv[2]);
-    } else if (option_f == 0 && option_t == 0){
+
+    } else if (option_f + option_t == 0){
         option = 11;
-        //choixHash(option, argv[1], "sha1");
-        calculHash(argv[1], "sha1");
-    } else {
+
+    for(i=0;i<argc-1;i++){
+        strcat(arr,argv[i+1]);
+        strcat(arr," ");
+
+
+    }
+
+    printf("%s\n",arr);
+
+          calculHash(arr, "sha1");
+          free(arr);
+        }
+
+     else {
         option = 0;
     }
 
