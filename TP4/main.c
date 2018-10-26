8fd57cb502f42e572339f8345d08af76481d67eb
ikam6 <marko93@bluewin.ch>
2018-10-26 22:05:05 +0200

    Listing de fichiers : OK. Reste la partie Backup à faire.

--- TP4/main.c
+++ TP4/main.c
@@ -18,41 +18,59 @@
 /////////////////////////////////////////////////////////////
 
 // TODO ca sert a rien la fonction IsADirectory... Il est deja dans listDir
+// int IsADirectory ( const char* name)//test si c'est un repertoire
+// {
+//     DIR* directory= opendir(name);
+//
+//     if(directory!= NULL)//c'est un repertoire
+//     {
+//         closedir(directory);
+//         return 0 ;
+//     }
+//
+//     if(errno==ENOTDIR)//Erreur, ce n'est pas un repertoire
+//     {
+//         return 1;
+//     }
+//
+//     return -1;//autre erreur
+// }
 
-int IsADirectory ( const char* name)//test si c'est un repertoire
-{
-    DIR* directory= opendir(name);
 
-    if(directory!= NULL)//c'est un repertoire
-    {
-        closedir(directory);
-        return 0 ;
-    }
+/////////////////////////////////////////////////////////////
+// FUNCTION formatDate
+/////////////////////////////////////////////////////////////
 
-    if(errno==ENOTDIR)//Erreur, ce n'est pas un repertoire
-    {
-        return 1;
-    }
-
-    return -1;//autre erreur
+char *formatDate(char *message, time_t val){
+    strftime(message, MAX_SIZE, " %d.%m.%Y %H:%M ", localtime(&val));
+    return message;
 }
 
 /////////////////////////////////////////////////////////////
-// FUNCTION IsADirectory
+// FUNCTION printInfo
 /////////////////////////////////////////////////////////////
 
-int printInfo (const char *nameDir, const char *nameFile)//affiche les données
+int printInfo(const char *nameDir, const char *nameFile)//affiche les données
 {
 
     char t[MAX_SIZE] = "";
     struct stat fileStat;
+    // printf("1. %s\n", nameDir);
+    // printf("2. %s\n", nameFile);
 
-    if(stat(nameDir,&fileStat) < 0) {
-        printf("1) Information for %s\n",nameDir);
-        return 1;
+    char strDir[MAX_SIZE]="";
+    strcat(strDir, nameDir);
+    strcat(strDir, "/"); // a faire attention selon l'entrée sur le terminal !! (risque de double //)
+    strcat(strDir, nameFile);
+    strcpy(t, strDir); //utile pour afficher le temps 
+    //printf("\nstringDir : %s\n", strDir);
+
+
+    if(stat(strDir, &fileStat) <0 ){
+        fprintf(stderr, "Cannot stat %s : %s\n", strDir, strerror(errno) );
+    }else  {
+        //printf("File inode: %ld\n", fileStat.st_ino);
     }
-
-    //printf("2) Information for %s, fichier %s\n",nameDir, nameFile);
 
 
     if ((S_ISDIR(fileStat.st_mode))== 0)
@@ -76,17 +94,9 @@ int printInfo (const char *nameDir, const char *nameFile)//affiche les données
     printf( (fileStat.st_mode & S_IWOTH) ? "w" : "-");
     printf( (fileStat.st_mode & S_IXOTH) ? "x" : "-");
 
-    printf(" %ld",sizeof(fileStat));
-    //affichage jour /mois /heure / année de modification
-    if (stat(nameFile, &fileStat)) {
-        strftime(t, 100, " %a %b %d %T %Y ", localtime( &fileStat.st_mtime));
-        printf(" %s", nameDir);
-        printf("/%s\n",nameFile);
-    }
-    else {
-        printf("-----------------\n");
-    }
-    //printf(" %s\n",nameDir);
+    printf(" %6ld",fileStat.st_size);
+    printf(" %s", formatDate(t, fileStat.st_mtime)); //
+    printf(" %s\n", strDir);
 }
 
 /////////////////////////////////////////////////////////////
@@ -111,9 +121,6 @@ static void listDir (const char *dirName){
         // Obtient le nom de l'entrée et affiche
         d_name = entry->d_name;
         printInfo(dirName, d_name);
-        // printf("-----------------\n");
-        // printf("%s/%s\n", dirName, d_name);
-
         //Est-ce que 'entry' est un sous-répertoire
         if (entry->d_type & DT_DIR) {
             //Est-ce que 'entry' n'est pas '..' ou '.'
@@ -122,13 +129,14 @@ static void listDir (const char *dirName){
                 //forme le nom du sous-répertoire et affiche
                 int path_length = snprintf (path, PATH_MAX,"%s/%s",
                 dirName, d_name);
-                printf("%s\n", path);
+                //printf("Dans la question if : %s\n", path);
                 //Vérifie que le nom du sous-répertoire n'est pas trop long
                 if (path_length >= PATH_MAX) {
                     fprintf(stderr, "Path length has got too long.\n");
                     exit(EXIT_FAILURE);
                 }
                 //Appel récursif
+                //printInfo(dirName, d_name);
                 listDir(path);
             }
         }
@@ -145,31 +153,31 @@ static void listDir (const char *dirName){
 // FUNCTION copyDir
 /////////////////////////////////////////////////////////////
 
-static void copyDir (const char *dir_in, const char *dir_out){
-    DIR *d_in = opendir(dir_in);
-    DIR *d_out = opendir(dir_out);
-    struct dirent *entry;
-    const char *d_name;   //nom d'une entrée
-
-    //En cas d'erreur d'ouverture
-    if (! d_in) {
-        fprintf(stderr, "Cannot open directory '%s': %s\n",
-        dir_in, strerror(errno));
-        exit(EXIT_FAILURE);
-    }
-
-    if (! d_out) {
-        printf("On va creer un dossier ... \n");
-        d_out = mkdir(dir_out, 0777);
-
-        if (d_out < 0) {
-            int savedError = errno;
-            closedir(d_in);
-            fprintf(stderr, "Could not open the file %s: %s\n",
-            dir_out, strerror(savedError));
-        }
-    }
-}
+// static void copyDir (const char *dir_in, const char *dir_out){
+//     DIR *d_in = opendir(dir_in);
+//     DIR *d_out = opendir(dir_out);
+//     struct dirent *entry;
+//     const char *d_name;   //nom d'une entrée
+//
+//     //En cas d'erreur d'ouverture
+//     if (! d_in) {
+//         fprintf(stderr, "Cannot open directory '%s': %s\n",
+//         dir_in, strerror(errno));
+//         exit(EXIT_FAILURE);
+//     }
+//
+//     if (! d_out) {
+//         printf("On va creer un dossier ... \n");
+//         d_out = mkdir(dir_out, 0777);
+//
+//         if (d_out < 0) {
+//             int savedError = errno;
+//             closedir(d_in);
+//             fprintf(stderr, "Could not open the file %s: %s\n",
+//             dir_out, strerror(savedError));
+//         }
+//     }
+// }
 
 /////////////////////////////////////////////////////////////
 // FUNCTION main
@@ -177,6 +185,6 @@ static void copyDir (const char *dir_in, const char *dir_out){
 
 int main (int argc, char *argv[]) {
     listDir(argv[1]);
-    copyDir(argv[1], argv[2]);
+    //copyDir(argv[1], argv[2]);
     return EXIT_SUCCESS;
 }
