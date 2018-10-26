ff6628d7841b4770178c38ed863eb2c0e6ff6c5e
ikam6 <marko93@bluewin.ch>
2018-10-27 00:35:58 +0200

    Fini pour ojd, Backup marche pas git status

--- TP4/main.c
+++ TP4/main.c
@@ -62,7 +62,7 @@ int printInfo(const char *nameDir, const char *nameFile)//affiche les données
     strcat(strDir, nameDir);
     strcat(strDir, "/"); // a faire attention selon l'entrée sur le terminal !! (risque de double //)
     strcat(strDir, nameFile);
-    strcpy(t, strDir); //utile pour afficher le temps 
+    strcpy(t, strDir); //utile pour afficher le temps
     //printf("\nstringDir : %s\n", strDir);
 
 
@@ -153,38 +153,115 @@ static void listDir (const char *dirName){
 // FUNCTION copyDir
 /////////////////////////////////////////////////////////////
 
-// static void copyDir (const char *dir_in, const char *dir_out){
-//     DIR *d_in = opendir(dir_in);
-//     DIR *d_out = opendir(dir_out);
-//     struct dirent *entry;
-//     const char *d_name;   //nom d'une entrée
-//
-//     //En cas d'erreur d'ouverture
-//     if (! d_in) {
-//         fprintf(stderr, "Cannot open directory '%s': %s\n",
-//         dir_in, strerror(errno));
-//         exit(EXIT_FAILURE);
-//     }
-//
-//     if (! d_out) {
-//         printf("On va creer un dossier ... \n");
-//         d_out = mkdir(dir_out, 0777);
-//
-//         if (d_out < 0) {
-//             int savedError = errno;
-//             closedir(d_in);
-//             fprintf(stderr, "Could not open the file %s: %s\n",
-//             dir_out, strerror(savedError));
-//         }
-//     }
-// }
+static void copyDir (const char *dir_in, const char *dir_out){
+    DIR *d_in = opendir(dir_in);
+    DIR *d_out = opendir(dir_out);
+    struct dirent *entry;
+    const char *d_name;   //nom d'une entrée
+    int err;
+
+    //En cas d'erreur d'ouverture
+    if (! d_in) {
+        fprintf(stderr, "Cannot open directory '%s': %s\n",
+        dir_in, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    if (! d_out) {
+        printf("On va creer un dossier %s\n", dir_out);
+        mkdir(dir_out, 0777);
+        printf("path dirOut: %s\n", dir_out);
+
+        if (d_out < 0) {
+            int savedError = errno;
+            closedir(d_in);
+            fprintf(stderr, "Could not open directory %s: %s\n",
+            dir_out, strerror(savedError));
+        }
+    }
+    printf("On a le dossier %s et on peut copier les %s dedans \n", dir_out, dir_in);
+    //printf( (link(dir_in, dir_out) == 0) ? "Succes\n" :"Fail\n" );
+
+
+    printf("dirIn %s, drirOut: %s\n", dir_in, dir_out);
+
+    while( (entry = readdir(d_in)) != NULL ) {
+        // Obtient le nom de l'entrée et affiche
+        d_name = entry->d_name;
+        //Est-ce que 'entry' est un sous-répertoire
+        if (entry->d_type & DT_DIR) {
+            //Est-ce que 'entry' n'est pas '..' ou '.'
+            if (strcmp(d_name, "..") != 0 && strcmp(d_name, ".") != 0) {
+                char path[PATH_MAX];
+                //forme le nom du sous-répertoire et affiche
+                int path_length = snprintf (path, PATH_MAX,"%s/%s",
+                dir_in, d_name);
+                //printf("Dans la question if : %s\n", path);
+                //Vérifie que le nom du sous-répertoire n'est pas trop long
+                if (path_length >= PATH_MAX) {
+                    fprintf(stderr, "Path length has got too long.\n");
+                    exit(EXIT_FAILURE);
+                }
+                //Appel récursif
+                //printInfo(dirName, d_name);
+                copyDir(path, dir_out);
+                printf("Hello\n" );
+            }
+        }
+        err = 1; //TODO aucune idee de ce machin
+        int n = err;
+        if (n==0) break;
+        char pathDir[]="";
+        strcat(pathDir, dir_in);
+        strcat(pathDir, "/");
+        strcat(pathDir, d_name);
+        printf("Notre nouveau path est %s\n", pathDir);
+
+        int fd = open(pathDir, O_CREAT, 0777);
+
+        if (fd < 0){
+            int savedError = errno;
+            fprintf(stderr, "Could not open directory %s: %s\n",
+            pathDir, strerror(savedError));
+        }
+
+        err = write(fd, d_name, n);
+        if (err == -1){
+            printf("Error writing file.\n" );
+            exit(EXIT_FAILURE);
+        }
+    } //while(1)
+
+    closedir(d_in);
+    closedir(d_out);
+
+    if( closedir(d_in) ) {
+        fprintf(stderr, "Could not close '%s': %s\n",
+        dir_in, strerror (errno));
+        exit (EXIT_FAILURE);
+    }
+}
 
 /////////////////////////////////////////////////////////////
 // FUNCTION main
 /////////////////////////////////////////////////////////////
 
 int main (int argc, char *argv[]) {
-    listDir(argv[1]);
+    if (argc > 2){
+        // boucle pour recuperer tout les dossiers/fichiers sauf le dernier
+        // qui sera le repertoire destination
+        for(int i=1;i<argc-1;i++){
+            //printf("%i :: %s\n", i, argv[i]);
+            copyDir(argv[i], argv[argc-1]);
+        }
+    }
+    else if (argc == 2) {
+        listDir(argv[1]);
+    }else{
+        printf("Rappel pour l'entrée : ./ultra-cp dossier(s) \n");
+        exit(EXIT_FAILURE);
+    }
+
     //copyDir(argv[1], argv[2]);
     return EXIT_SUCCESS;
 }
