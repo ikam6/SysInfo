ecdcf9b157071304254035ff55b6a5d6dc69014a
alexialid <alexia.lienhard@etu.unige.ch>
2018-11-20 15:29:27 +0100

    ca marche de ouf!! faut mettre au propre et faire le makefile et faire la lecture du fichier mail

--- TP6/essai.c
+++ TP6/essai.c
@@ -16,10 +16,9 @@
 #define HELO "HELO 192.168.1.1\r\n"
 #define DATA "DATA\r\n"
 #define QUIT "QUIT\r\n"
+#define MAX 256
 
-//#define h_addr h_addr_list[0]
-
-	//FILE *fin;
+/
 
 
 struct sockaddr_in server;
@@ -28,35 +27,25 @@ struct hostent *gethostbyname();
 
 int sock;
 char *host_id="smtp.unige.ch";
-char *from_id="alexia.lienhard@etu.unige.ch";
 
-char *to_id="alexia.lienhard@etu.unige.ch";
-char *sub="testmail\r\n";
+const char sub[MAX];
 char wkstr[100]="hello how r u\r\n";
 
-
-/*=====Create Socket=====*/
-int createSocket(){
-	 sock = socket(AF_INET, SOCK_STREAM, 0);
-
-if (sock==-1)	{
-	 perror("opening stream socket");
-	 exit(1);
-	}
-	else
-		printf( "socket created\n");
-		return sock;
+void die(char *issue) {
+  perror(issue);
+  exit(EXIT_FAILURE);
 }
 
-//=====Read a string from the socket=====*/
 void read_socket()
 {
 	char buf[BUFSIZ+1];
 	int len;
 
 	len = read(sock,buf,BUFSIZ);
+
 	write(1,buf,len);
-	printf("Server:%s\n",buf);
+	memset(buf,1, sizeof(buf));
+	//printf("Server:%s\n",buf);
 }
 
 /*=====Send a string to the socket=====*/
@@ -64,28 +53,42 @@ void send_socket(char *s)	{
 	write(sock,s,strlen(s));
 	write(1,s,strlen(s));
 		//printf("Client:%s\n",s);
-
 		}
 
 void SendEmail(char* from_id, char* to_id, char* sub, char* wkstr ){
 	/*=====Write some data then read some =====*/
-	read_socket(); /* SMTP Server logon string */
-	send_socket(HELO); /* introduce ourselves */
-	read_socket(); /*Read reply */
+/* SMTP Server logon string */
+	read_socket();
+
+	/* introduce ourselves */
+	send_socket(HELO);
+
+	/*Read reply */
+	read_socket();
+
+	/*intruduce sender*/
 	send_socket("MAIL FROM: ");
 	send_socket(from_id);
 	send_socket("\r\n");
-	read_socket(); /* Sender OK */
+
+	 /* sender OK */
+	read_socket();
 	send_socket("VRFY ");
 	send_socket(from_id);
 	send_socket("\r\n");
-	read_socket(); // Sender OK */
+	read_socket();
+
+	/*introduce Recipient*/
 	send_socket("RCPT TO: "); /*Mail to*/
 	send_socket(to_id);
 	send_socket("\r\n");
-	read_socket(); // Recipient OK*/
-	send_socket(DATA);// body to follow*/
-	send_socket("Subject: ");
+
+	/* Recipient OK*/
+	read_socket();
+
+	// body to follow*/
+	send_socket(DATA);
+	send_socket("subject: ");
 	send_socket(sub);
 	read_socket(sock); // Recipient OK*/
 	send_socket(wkstr);
@@ -99,86 +102,59 @@ close(sock);
 exit(0);
 
 		}
-
 	/*=====MAIN=====*/
 
 int main(int argc, char* argv[]){
+char* from_id;
+char* to_id;
+char* sub;
 
-		    /*=====Create Socket=====*/
+/*=====Initialisation======*/
+	//TODO mettre a 5, et verifier le file.txt
+	//TODO mettre exemple 
+	if (argc != 4) {
+		fprintf(stderr, "USAGE: %s <Email address FROM> <Email addres TO> <subject> <File.txt>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
 
-		    sock = socket(AF_INET, SOCK_STREAM, 0);
+	from_id = argv[1];
+	to_id = argv[2];
+	sub= argv[3];
+	//file = argv[3];
 
-		    if (sock==-1)
+/*=====Create Socket=====*/
+sock = socket(AF_INET, SOCK_STREAM, 0);
 
-		    {
+	    if (sock==-1)
+	    {
+	     perror("opening stream socket");
+	     exit(1);
+	    }
 
-		     perror("opening stream socket");
+	    else
 
-		     exit(1);
+	      printf ("socket created\n");
 
-		    }
-			    else
-
-		      printf(  "socket created\n");
-
-		    /*=====Verify host=====*/
-
-		    server.sin_family = AF_INET;
-
-		    hp = gethostbyname(host_id);
-
-		    if (hp==(struct hostent *) 0)
-
-		    {
-
-		     fprintf(stderr, "%s: unknown host\n", host_id);
-
-		     exit(2);
-
-		    }
-
-		    /*=====Connect to port 25 on remote host=====*/
-
-		    memcpy((char *) &server.sin_addr, (char *) hp->h_addr, hp->h_length);
-
-		    server.sin_port=htons(25); /* SMTP PORT */
-
-		    if (connect(sock, (struct sockaddr *) &server, sizeof server)==-1)
-
-		    {
-
-		     perror("connecting stream socket");
-
-		     exit(1);
-
-		    }
-
-		    else
-
-		     printf( "Connected\n");
-/*	createSocket(sock);
-printf("je fais tout comme il faut");
-		/*=====Verify host=====*/
-	/*server.sin_family = AF_INET;
-		hp = gethostbyname(host_id);
-
-		if (hp==(struct hostent *) 0)
-		{
-		 fprintf(stderr, "%s: unknown host\n", host_id);
+ /*=====Verify host=====*/
+server.sin_family = AF_INET;
+hp = gethostbyname(host_id);
+if (hp==(struct hostent *) 0)
+ {
+ 		fprintf(stderr, "%s: unknown host\n", host_id);
 		 exit(2);
-		}
-printf("je suis la");
-		/*=====Connect to port 25 on remote host=====*/
-	/*	memcpy((char *) &server.sin_addr, (char *) hp->h_addr, hp->h_length);
-		server.sin_port=htons(25); /* SMTP PORT */
-/*
-	if (connect(sock, (struct sockaddr *) &server, sizeof server)==-1)
-	{
-		perror("connecting stream socket");
-		exit(1);
-		}
-		else
-			printf( "Connected\n");*/
+   }
+
+ /*=====Connect to port 25 on remote host=====*/
+ memcpy((char *) &server.sin_addr, (char *) hp->h_addr, hp->h_length);
+
+ server.sin_port=htons(25); /* SMTP PORT */
+ if (connect(sock, (struct sockaddr *) &server, sizeof server)==-1)
+  {
+  	perror("connecting stream socket");
+ 		exit(1);
+    }
+ 		else
+  		printf( "Connected\n");
 
 			SendEmail(from_id, to_id, sub, wkstr);
 		}
