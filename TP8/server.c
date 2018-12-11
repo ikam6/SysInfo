537c525bd71ba4402fc55aa3ef80c2d518db8817
alexialid <alexia.lienhard@etu.unige.ch>
2018-12-11 16:00:46 +0100

    création tp8

---
+++ TP8/server.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <semaphore.h>
